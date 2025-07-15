143b3a42220ae5292122969d10e05c121bc2f094
Sasmitha Hiram <sasmitha.hiram@helixzsolutions.com>
2025-07-15 13:38:14 +0530

    cache

---
+++ app/components/ProductCard.jsx
@@ -0,0 +1,10 @@
+'use client'
+const ProductCard = () => {
+    return (
+    <div>
+        <button onClick={() => console.log('clicked')}>Product Card</button>
+    </div>
+    )
+}
+
+export default ProductCard
