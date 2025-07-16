60e0e0009bc00eab5ec369f0a64337c8a61945e2
Sasmitha Hiram <sasmitha.hiram@helixzsolutions.com>
2025-07-16 09:29:05 +0530

    f

---
+++ app/users/UserTable.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+interface User {
+  id: number;
+  name: string;
+}
+const UserTable = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: { revalidate: 10 },
+  });
+  const users: User[] = await res.json();
+  return (
+    <div>
+        <h1>Users</h1>
+
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </div>
+  )
+}
+
+export default UserTable
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,19 +1,15 @@
+import UserTable from "./UserTable";
+
 interface User {
   id: number;
   name: string;
 }
 const UserPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    next: { revalidate: 10 },
-  });
-  const users: User[] = await res.json();
+  
   return (
     <>
-      <h1>Users</h1>
-
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
+    <h1></h1>
+      <UserTable/>
     </>
   );
 };
